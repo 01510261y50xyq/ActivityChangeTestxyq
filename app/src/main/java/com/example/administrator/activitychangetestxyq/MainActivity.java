6649e59522bf435658b8fd5f97a51c193b86970c
01510261y50xyq <1052543361@qq.com>
2018-10-17 16:39:43 +0800

    Initial commit

---
+++ app/src/main/java/com/example/administrator/activitychangetestxyq/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.administrator.activitychangetestxyq;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.buttonA).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, SecondActivity.class));
+            }
+        });
+    }
+}
